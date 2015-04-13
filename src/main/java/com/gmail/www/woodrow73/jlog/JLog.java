7c95964f81847427fbbdc814ca51f9aaf7ca72db
Justin W. Flory <me@justinwflory.com>
2015-04-12 21:32:05 -0400

    Add JavaDoc annotation for Alex as the author

--- src/main/java/com/gmail/www/woodrow73/jlog/JLog.java
+++ src/main/java/com/gmail/www/woodrow73/jlog/JLog.java
@@ -1,18 +1,20 @@
-package com.gmail.www.woodrow73.jlog;
+/*
+ * Copyright 2015 CrystalCraftMC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.io.PrintWriter;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+package com.gmail.www.woodrow73.jlog;
 
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
@@ -24,6 +26,13 @@ import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.*;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+/**
+ * @author Alex Woodward (Jwood)
+ */
 public class JLog extends JavaPlugin implements Listener {
 	
 	/**Holds the current list of commands/chat logs to upload to the database (unloads at 21)*/
